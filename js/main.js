86b013562f35f3a4443a1841a1d3d58079ff794a
Ross <ross@Rosss-iMac.local>
2017-10-26 17:46:49 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+// wdi-fundamentals-memorygame 
+// Built by Ross Lillis.
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push("queen");
+cardsInPlay.push("king");
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You've found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
+}
