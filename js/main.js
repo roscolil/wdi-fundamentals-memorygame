ef2cb7adc01b17d9fe0355b06ee76b5729d41d5f
Ross <ross@Rosss-iMac.local>
2017-11-01 14:40:17 +1100

    memory game enhancements

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // wdi-fundamentals-memorygame
-// Developed by Ross Lillis.
+// Developed by Ross Lillis for GA.
 
 
 var cards = [
@@ -27,20 +27,25 @@ var cards = [
 
 var cardsInPlay = [];
 
-// Checks for a match between the 2 chosen cards and displays 
-// the appropriate message.
+
+/* Checks for a match between the 2 chosen cards and displays 
+   the appropriate message. */
+
 var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
+    winScore();
   }
   else {
     alert("Sorry, try again.");
+    lossScore();
   }
 };
 
-// Assigns attribute to cardId variable and logs message to console 
-// while adding this value to cardsInPlay array. If two cards are played
-// it calls the checkForMatch function.
+/* Assigns attribute to cardId variable and logs message to console 
+   while adding this value to cardsInPlay array. If two cards are played
+   it calls the checkForMatch function. */
+
 var flipCard = function() {
   var cardId = this.getAttribute('data-id');
 
@@ -56,8 +61,9 @@ var flipCard = function() {
   }
 };
 
-// On click, calls flipCard function and creates image elements for 
-// the length of the cards array.
+/* On click, calls flipCard function and creates image elements for 
+   the length of the cards array. */
+
 var createBoard = function() {
 
   for (var i = 0; i < cards.length; i++) {
@@ -71,9 +77,35 @@ var createBoard = function() {
   }
 };
 
+// Win/loss score counters.
+
+var winScore = function() {
+  var wins = 0;
+  wins += 1;
+  document.getElementById('wins').innerHTML = wins;
+};
+
+var lossScore = function() {
+  var losses = 0;
+  losses += 1;
+  document.getElementById('losses').innerHTML = losses;
+};
+
+// Onclick event resets game board images.
+
+  var resetBoard = function() {
+    alert("It works!!");
+/* Tried using this code within the function to see if there is a way 
+to reset the game board without reloading the page (and losing the score). 
+Not sure if i'm on the right track here. */
+    
+//    var gameBoard = document.getElementById('game-board');
+//    var child = document.getElementsByTagName('img');
+//    gameBoard.removeChild(child);
+};
+
 //Calling main function.
 createBoard(); 
 
-
-
-
+var resetButton = document.querySelector('button');
+button.addEventListener('click', resetBoard);
